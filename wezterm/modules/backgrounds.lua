842e96664db7d322a8c30bc8bb9aac5254b903aa
ajason13 <a.jason13@gmail.com>
2026-07-07 21:08:38 -0700

    Replace ball slide background with floor slide

--- wezterm/modules/backgrounds.lua
+++ wezterm/modules/backgrounds.lua
@@ -36,7 +36,7 @@ local background_files = {
   '200-anime/296-haikyuu-jump-block.png',
   '200-anime/297-haikyuu-ball-action.png',
   '200-anime/298-haikyuu-pointing-black.png',
-  '200-anime/299-haikyuu-ball-net-slide.png',
+  '200-anime/300-haikyuu-floor-slide.png',
 }
 
 local background_hsb = {
