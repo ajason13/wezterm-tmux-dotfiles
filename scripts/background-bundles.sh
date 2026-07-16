842acf1666fa0d9225997f6d378db9a8a0fcc635
ajason13 <a.jason13@gmail.com>
2026-07-15 21:01:05 -0700

    Raise anime background bundle cap

--- scripts/background-bundles.sh
+++ scripts/background-bundles.sh
@@ -4,6 +4,6 @@ background_bundles() {
   cat <<'EOF'
 general|backgrounds-general|000-general|16777216
 vehicles|backgrounds-vehicles|100-vehicles|25165824
-anime|backgrounds-anime|200-anime|75497472
+anime|backgrounds-anime|200-anime|92274688
 EOF
 }
